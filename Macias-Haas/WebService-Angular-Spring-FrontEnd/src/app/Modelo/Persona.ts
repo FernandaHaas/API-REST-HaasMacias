93e2e202ada2c5f2771b7229dfb8813f0a269b66
FernandaHaas <up190671@alumnos.upa.edu.mx>
2022-05-25 00:11:22 -0500

    .

---
+++ Macias-Haas/WebService-Angular-Spring-FrontEnd/src/app/Modelo/Persona.ts
@@ -0,0 +1,6 @@
+export class Persona{
+    id:number;
+    email: string;
+    name:String;
+    priority:number;
+}
---
+++ Macias-Haas/WebService-Angular-Spring-FrontEnd/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  Url: 'http://ec2-3-82-48-4.compute-1.amazonaws.com:8080/usuario'
+};
